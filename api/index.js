673929ae14ac98913929a95094c93a17b92b64ce
Corey Wendling <coreywendling@Coreys-MacBook-Air.local>
2016-12-15 21:34:32 -0500

    Updated readme and added web and mongo

--- api/index.js
+++ api/index.js
@@ -1,1 +1,31 @@
-console.log('hello');
+var express =  require('express');
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+
+var dbName = 'mongo';
+var tableName = 'test';
+
+app.get('/', function(req, res) {
+  db.collection(tableName).find().toArray(function(err, results) {
+    console.log(results);
+    res.send(results);
+  });
+});
+
+app.post('/', function(req, res) {
+	db.collection(tableName).save(req.body, function(req, res) {
+		if(err) {
+			return console.log('Error saving...');
+		}
+	});
+});
+
+MongoClient.connect('mongodb://localhost:27017/'+dbName, function(err, db) {
+  if(err) {
+    console.log('Mongo connect error... ' + err);
+  }
+
+  app.listen(8888, function() {
+    console.log('Listening on port 8888');
+  });
+});
