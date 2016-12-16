ae4fdac4e01740e24f39a0fee6a1d607dcbbff24
Corey Wendling <coreywendling@Coreys-MacBook-Air.local>
2016-12-15 23:32:26 -0500

    Changed mongo url to use docker image name

--- api/index.js
+++ api/index.js
@@ -7,28 +7,29 @@ var tableName = 'test';
 
 var db;
 
-app.get('/', function(req, res) {
-  db.collection(tableName).find().toArray(function(err, results) {
-    console.log(results);
-    res.send(results);
-  });
-});
-
-app.post('/', function(req, res) {
-	db.collection(tableName).save(req.body, function(req, res) {
-		if(err) {
-			return console.log('Error saving...');
-		}
-	});
-});
-
-MongoClient.connect('mongodb://localhost:27017/'+dbName, function(err, database) {
+MongoClient.connect('mongodb://mongo:27017', function(err, database) {
   if(err) {
-    console.log('Mongo connect error... ' + err);
+    return console.log('Mongo connect error... ' + err);
   }
   db = database;
-  
+
+  app.get('/', function(req, res) {
+    db.collection(tableName).find().toArray(function(err, results) {
+      console.log(results);
+      res.send(results);
+    });
+  });
+
+  app.post('/', function(req, res) {
+  	db.collection(tableName).save(req.body, function(req, res) {
+  		if(err) {
+  			return console.log('Error saving...');
+  		}
+  	});
+  });
+
   app.listen(8888, function() {
     console.log('Listening on port 8888');
   });
 });
+
