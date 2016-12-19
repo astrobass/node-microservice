1ab27bd6bc673767c24029b39de5f13b78525038
Astro Bass <github@wtree.com>
2016-12-19 11:16:43 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,20 +13,7 @@ MongoClient.connect('mongodb://mongo:27017', function(err, database) {
   }
   db = database;
 
-  app.get('/', function(req, res) {
-    db.collection(tableName).find().toArray(function(err, results) {
-      console.log(results);
-      res.send(results);
-    });
-  });
-
-  app.post('/', function(req, res) {
-  	db.collection(tableName).save(req.body, function(req, res) {
-  		if(err) {
-  			return console.log('Error saving...');
-  		}
-  	});
-  });
+  app.use('/songs', songs);
 
   app.listen(8888, function() {
     console.log('Listening on port 8888');
