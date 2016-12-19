3595c5629b3a7b70a9f65ff932b86d4f0de5da83
Astro Bass <github@wtree.com>
2016-12-19 16:09:27 -0500

    Added comments

--- api/index.js
+++ api/index.js
@@ -1,25 +1,28 @@
 var express =  require('express');
-var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
 var songs = require('./songs');
 
-console.log('Database: ' + config.db[app.settings.env]);
+var app = express();
 
+// Connect to Mongo database
 mongoose.Promise = global.Promise;
 mongoose.connect(config.db[app.settings.env], function(err) {
   if(err) {
     return console.log('Mongo connect error... ' + err);
   }
+  
+  // Make database accessable to API resources
   var db = mongoose.connection;
-
   app.use(function(req, res, next){
     req.db = db;
     next();
   });
 
+  // List of resources
   app.use('/songs', songs);
 
+  // Start API server
   app.listen(8888, function() {
     console.log('Listening on port 8888');
   });
