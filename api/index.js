63c2764eb39ed43a7b2bd8d89cda46243a8ce865
Astro Bass <github@wtree.com>
2016-12-19 11:49:27 -0500

    Fixed typo and added config

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 var express =  require('express');
 var app = express();
-var mongoose - require('mongoose');
+var mongoose = require('mongoose');
+var config = require('./config');
 
 var dbName = 'mongo';
 var tableName = 'test';
