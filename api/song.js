68a90db22aa88029d7ef0b00b513386fb6dd9170
Astro Bass <github@wtree.com>
2016-12-19 11:29:22 -0500

    Update song.js

--- api/song.js
+++ api/song.js
@@ -3,25 +3,48 @@ var router = express.Router();
 
 // List
 router.get('/', function(req, res) {
-  res.send('LIST');
+  db.collection(tableName).find().toArray(function(err, results) {
+    if(err) {
+      console.log('Error');
+    }
+    res.send('LIST');
+  });
 }
 
 // Create
 router.post('/', function(req, res) {
-  res.send('CREATE');
+  db.collection(tableName).find().toArray(function(err, results) {
+    if(err) {
+      console.log('Error');
+    }
+    res.send('CREATE');
+  });
 }
 
 // Read
 router.get('/:id', function(req, res) {
-  res.send('READ');
+  db.collection(tableName).find().toArray(function(err, results) { 
+    if(err) {
+      console.log('Error');
+    }
+    res.send('READ');
+  });
 }
 
 // Update
 router.post('/:id', function(req, res) {
-  res.send('UPDATE');
+  db.collection(tableName).save(req.body, function(req, res) {
+    if(err) {
+      console.log('Error');
+    }
+    res.send('UPDATE');
+  });
 }
 
 // DELETE
 router.delete('/:id', function(req, res) {
+    if(err) {
+      console.log('Error');
+    }
   res.send('DELETE');
 }
